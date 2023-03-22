001c79b6f87ff33bbea67e01e209cd955b6743ed
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2023-03-22 23:23:12 +0530

    impv

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -14,28 +14,28 @@ export default function Dashboard() {
     <div className="flex w-full justify-center">
       <Head></Head>
       <main className="w-full max-w-5xl mt-24 flex flex-wrap gap-10 justify-center">
-        <div className="p-10 w-11/12 sm:w-72 text-2xl text-center font-semibold text-black bg-tint-emerald rounded-lg flex flex-col justify-center items-center gap-6 cursor-pointer hover:ring-4 ring-offWhite/40">
-          <Sliders className="w-20 h-20" />
+        <div className="group p-10 w-11/12 sm:w-72 text-2xl text-center font-semibold text-black bg-tint-emerald rounded-lg flex flex-col justify-center items-center gap-6 cursor-pointer hover:ring-4 ring-offWhite/40">
+          <Sliders className="w-20 h-20 group-hover:scale-90 transition-all ease-in-out" />
           Tailwind Palette Generator
         </div>
-        <div className="p-10 w-11/12 sm:w-72 text-2xl text-center font-semibold text-black bg-tint-yellow rounded-lg flex flex-col justify-center items-center gap-6 cursor-pointer hover:ring-4 ring-offWhite/40">
-          <Fire className="w-20 h-20" />
+        <div className="group p-10 w-11/12 sm:w-72 text-2xl text-center font-semibold text-black bg-tint-yellow rounded-lg flex flex-col justify-center items-center gap-6 cursor-pointer hover:ring-4 ring-offWhite/40">
+          <Fire className="w-20 h-20 group-hover:scale-90 transition-all ease-in-out" />
           Trending Color Palettes
         </div>
-        <div className="p-10 w-11/12 sm:w-72 text-2xl text-center font-semibold text-black bg-tint-fuchsia rounded-lg flex flex-col justify-center items-center gap-6 cursor-pointer hover:ring-4 ring-offWhite/40">
-          <Robot className="w-20 h-20" />
+        <div className="group p-10 w-11/12 sm:w-72 text-2xl text-center font-semibold text-black bg-tint-fuchsia rounded-lg flex flex-col justify-center items-center gap-6 cursor-pointer hover:ring-4 ring-offWhite/40">
+          <Robot className="w-20 h-20 group-hover:scale-90 transition-all ease-in-out" />
           Palette from Image [AI]
         </div>
-        <div className="p-10 w-11/12 sm:w-72 text-2xl text-center font-semibold text-black bg-tint-teal rounded-lg flex flex-col justify-center items-center gap-6 cursor-pointer hover:ring-4 ring-offWhite/40">
-          <Palette2 className="w-20 h-20" />
+        <div className="group p-10 w-11/12 sm:w-72 text-2xl text-center font-semibold text-black bg-tint-teal rounded-lg flex flex-col justify-center items-center gap-6 cursor-pointer hover:ring-4 ring-offWhite/40">
+          <Palette2 className="w-20 h-20 group-hover:scale-90 transition-all ease-in-out" />
           Color Palette from Color [AI]
         </div>
-        <div className="p-10 w-11/12 sm:w-72 text-2xl text-center font-semibold text-black bg-tint-red rounded-lg flex flex-col justify-center items-center gap-6 cursor-pointer hover:ring-4 ring-offWhite/40">
-          <ArrowLeftRight className="w-20 h-20" />
+        <div className="group p-10 w-11/12 sm:w-72 text-2xl text-center font-semibold text-black bg-tint-red rounded-lg flex flex-col justify-center items-center gap-6 cursor-pointer hover:ring-4 ring-offWhite/40">
+          <ArrowLeftRight className="w-20 h-20 group-hover:scale-90 transition-all ease-in-out" />
           Color Code Converter
         </div>
-        <div className="p-10 w-11/12 sm:w-72 text-2xl text-center font-semibold text-black bg-tint-green rounded-lg flex flex-col justify-center items-center gap-6 cursor-pointer hover:ring-4 ring-offWhite/40">
-          <EmojiSunglasses className="w-20 h-20" />
+        <div className="group p-10 w-11/12 sm:w-72 text-2xl text-center font-semibold text-black bg-tint-green rounded-lg flex flex-col justify-center items-center gap-6 cursor-pointer hover:ring-4 ring-offWhite/40">
+          <EmojiSunglasses className="w-20 h-20 group-hover:scale-90 transition-all ease-in-out" />
           What Color Says!
         </div>
       </main>
