ed71fcd2f6d28af46c904e6ea3fb2a66c7d78a42
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2023-03-22 18:43:38 +0530

    added color scheme

--- src/components/globals/NavBar.js
+++ src/components/globals/NavBar.js
@@ -5,7 +5,7 @@ export default function NavBar() {
     <div className="p-4 flex w-full items-center justify-between">
       <Link href="/" className="flex items-center">
         <img src="logo.png" alt="" className="w-12 h-12" />
-        <span className="text-3xl font-bold text-[#E2D59A]">ColPat</span>
+        <span className="text-3xl font-bold text-yellow">ColPat</span>
       </Link>
       <div className="">{/* right side */}</div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+      colors:{
+        "yellow":"#E2D28F",
+        "black": "#000000",
+        "white": "#e5e5e5",
+        "offWhite": "#8B8880",
+        "grey": "#26221D",
+        "tint":{
+          "yellow": "#B9C2B6",
+          "emerald": "#B0D2C2",
+          "fuchsia": "#BBB7C1",
+          "green": "#6EC899",
+          "red": "#FC8262",
+          "yellow": "#F8D593",
+          "teal": "#D3F1F9",
+        }
+      },
   },
   plugins: [],
 };
