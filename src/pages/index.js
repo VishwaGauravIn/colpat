c4295c60c455cfacfc828cc7bc3ae6d1218bf76c
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2023-03-22 14:15:54 +0530

    Tailwind installation

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main></main>
+      <main className=""></main>
     </>
   );
 }
