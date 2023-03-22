2b17e50d2a4bdecd14c6ce5f803d3d82bc9f4c07
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2023-03-22 23:18:36 +0530

    basic dashboard design + impv

--- src/components/globals/NavBar.js
+++ src/components/globals/NavBar.js
@@ -3,8 +3,8 @@ import Link from "next/link";
 export default function NavBar() {
   return (
     <div className="p-4 flex w-full items-center justify-between">
-      <Link href="/" className="flex items-center">
-        <img src="logo.png" alt="" className="w-12 h-12" />
+      <Link href="/" className="flex items-center outline-none">
+        <img src="logo.png" alt="" className="w-12 h-12 pointer-events-none" draggable="false" />
         <span className="text-3xl font-bold text-yellow">ColPat</span>
       </Link>
       <div className="">{/* right side */}</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,8 +12,8 @@ module.exports = {
           "yellow": "#B9C2B6",
           "emerald": "#B0D2C2",
           "fuchsia": "#BBB7C1",
-          "green": "#6EC899",
-          "red": "#FC8262",
+          "green": "#8CD3AE",
+          "red": "#FEC2B2",
           "yellow": "#F8D593",
           "teal": "#D3F1F9",
         }
