80c727dfe48e6a78f1f76d703b5753c970dbd9ac
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2023-03-25 23:11:19 +0530

    added customToast

--- src/components/elements/toast/Toast.js
+++ src/components/elements/toast/Toast.js
@@ -30,3 +30,16 @@ export function codeCopiedToast() {
     theme: "colored",
   });
 }
+
+export function customToast(text) {
+  toast.info(text, {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "colored",
+  });
+}
