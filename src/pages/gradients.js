edc46950aa6337e085b12184b602676b3d221be0
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2023-03-29 15:21:36 +0530

    ux impv

--- src/pages/gradients.js
+++ src/pages/gradients.js
@@ -25,14 +25,14 @@ export default function Gradients() {
           ></div>
           <div className="flex justify-evenly w-full my-10">
             <button
-              className="rounded-full py-3 px-6 text-sm sm:text-base font-semibold bg-tint-teal text-black flex justify-center items-center gap-2 group hover:ring-4 ring-offWhite/40"
+              className="rounded-full py-3 px-6 text-sm sm:text-base font-semibold bg-tint-teal text-black flex justify-center items-center gap-2 group hover:ring-4 ring-offWhite/40 active:scale-95 transition-all ease-in-out"
               onClick={() => setBackground(gradient(Math.random()))}
             >
               <ArrowRepeat className="h-6 w-6 group-hover:scale-90 transition-all ease-in-out" />
               Generate New
             </button>
             <button
-              className="rounded-full py-3 px-6 text-sm sm:text-base font-semibold bg-tint-green text-black flex justify-center items-center gap-2 group hover:ring-4 ring-offWhite/40"
+              className="rounded-full py-3 px-6 text-sm sm:text-base font-semibold bg-tint-green text-black flex justify-center items-center gap-2 group hover:ring-4 ring-offWhite/40 active:scale-95 transition-all ease-in-out"
               onClick={() => codeCopier(`background-color: ${background};`)}
             >
               <ClipboardCheck className="h-6 w-6 group-hover:scale-90 transition-all ease-in-out" />
