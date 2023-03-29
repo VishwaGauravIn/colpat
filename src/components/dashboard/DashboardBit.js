f791e43eadee5d90829774a2acd23db8a607764c
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2023-03-29 16:46:29 +0530

    ui impv

--- src/components/dashboard/DashboardBit.js
+++ src/components/dashboard/DashboardBit.js
@@ -5,7 +5,7 @@ export default function DashboardBit({ color, label, children, href = "" }) {
   return (
     <Link href={href}>
       <div
-        className={`group p-10 w-11/12 sm:w-72 text-2xl text-center font-semibold text-black rounded-lg flex flex-col justify-center items-center gap-6 cursor-pointer hover:ring-4 ring-offWhite/40 ${color} `}
+        className={`group p-10 w-72 text-2xl text-center font-semibold text-black rounded-lg flex flex-col justify-center items-center gap-6 cursor-pointer hover:ring-4 ring-offWhite/40 ${color} `}
       >
         {children}
         {label}
