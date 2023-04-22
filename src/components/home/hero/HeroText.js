4902300ce06e270efc44df20dada6a056daf5274
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2023-04-23 02:01:40 +0530

    animation impv

--- src/components/home/hero/HeroText.js
+++ src/components/home/hero/HeroText.js
@@ -7,21 +7,21 @@ export default function HeroText() {
       <div className="md:flex flex-col gap-6 hidden">
         <Phrase color="#FEC2B2" margin={10}>
           <Char char="A" />
-          <Char char="I" />
+          <Char char="I" delay={0.5} />
         </Phrase>
         <Phrase color="#D3F1F9" margin={10}>
           <Char char="M" />
-          <Char char="E" />
-          <Char char="E" />
-          <Char char="T" />
-          <Char char="S" />
+          <Char char="E" delay={0.5} />
+          <Char char="E" delay={1} />
+          <Char char="T" delay={1.5} />
+          <Char char="S" delay={2} />
         </Phrase>
         <Phrase color="#B0D2C2" margin={10}>
           <Char char="C" />
-          <Char char="O" />
-          <Char char="L" />
-          <Char char="O" />
-          <Char char="R" />
+          <Char char="O" delay={0.5} />
+          <Char char="L" delay={1} />
+          <Char char="O" delay={1.5} />
+          <Char char="R" delay={2} />
         </Phrase>
       </div>
       <div className="flex flex-col gap-6 p-10 pt-24 md:hidden">
