a64b4f1cad35d51932444fa5ddb78422008a203e
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2023-04-23 06:12:15 +0530

    basic meta tags

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,7 +5,43 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="title" content="ColPat : Color Palette & Design Tool" />
+        <meta
+          name="description"
+          content="ColPat is the ultimate color palette generator with awesome tools. ColPat's AI, user-friendly interface, and customization options make it easy to find the perfect colors. Unleash your creativity and elevate your designs with ColPat!"
+        />
+        <meta name="copyright" content="VishwaGauravIn" />
+        <meta
+          name="keywords"
+          content="ColPat, Color Palette, Gradient, Shade Generator, Color Shade, Tailwind"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="colpat.itsvg.in" />
+        <meta
+          property="og:title"
+          content="ColPat : Color Palette & Design Tool"
+        />
+        <meta
+          property="og:description"
+          content="ColPat is the ultimate color palette generator with awesome tools. ColPat's AI, user-friendly interface, and customization options make it easy to find the perfect colors. Unleash your creativity and elevate your designs with ColPat!"
+        />
+        <meta property="og:image" content="https://colpat.itsvg.in/og.png" />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="colpat.itsvg.in" />
+        <meta
+          property="twitter:title"
+          content="ColPat : Color Palette & Design Tool"
+        />
+        <meta
+          property="twitter:description"
+          content="ColPat is the ultimate color palette generator with awesome tools. ColPat's AI, user-friendly interface, and customization options make it easy to find the perfect colors. Unleash your creativity and elevate your designs with ColPat!"
+        />
+        <meta
+          property="twitter:image"
+          content="https://colpat.itsvg.in/og.png"
+        />
+      </Head>
       <body>
         <NavBar />
         <Main />
