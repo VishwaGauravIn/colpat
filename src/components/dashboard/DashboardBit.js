fc2cbc03b490f1f15b962e38b4a65469e434acc4
Suad Wolgram <72341912+swolg@users.noreply.github.com>
2023-10-16 18:04:47 +0200

    fix: card size

--- src/components/dashboard/DashboardBit.js
+++ src/components/dashboard/DashboardBit.js
@@ -5,7 +5,7 @@ export default function DashboardBit({ color, label, children, href = "" }) {
   return (
     <Link href={href}>
       <div
-        className={`group p-10 w-72 text-2xl text-center font-semibold text-black rounded-lg flex flex-col justify-center items-center gap-6 cursor-pointer hover:ring-4 ring-offWhite/40 ${color} `}
+        className={`group p-10 w-72 h-64 text-2xl text-center font-semibold text-black rounded-lg flex flex-col justify-center items-center gap-6 cursor-pointer hover:ring-4 ring-offWhite/40 ${color} `}
       >
         {children}
         {label}
